1ace6ab8b4976eca26e7cab3708830312f1424ee
dilee <dilee@trenditplatform.co.kr>
2018-02-22 14:20:57 +0900

    head keyword

--- resources/views/common/head.blade.php
+++ resources/views/common/head.blade.php
@@ -2,7 +2,9 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 <meta name="csrf-token" content="{{ csrf_token() }}" />
-<meta name="description" content="가상화폐 거래소 간 가격 비교 BTC, BTH, ETH, ETC, XRP, LTC, BTG, ZEC"/>
+<meta name="keywords" content="가상화폐 가격 비교, BTC, BTH, ETH, ETC, XRP, LTC, BTG, ZEC, 존버, 존버채팅, 김치프리미엄, 김프, 비트코인, 비트, 김프사이트, 리플, 이더리움"/>
+<meta name="description" content="가상화폐 비트코인 존버들의 채팅방, 김치프리미엄 계산"/>
 <meta property="og:type" content="website"/>
-<meta property="og:description" content="가상화폐 거래소 간 가격 비교 BTC, BTH, ETH, ETC, XRP, LTC, BTG, ZEC"/>
+<meta property="og:description" content="가상화폐 비트코인 존버들의 채팅방, 김치프리미엄 계산"/>
 <meta property="og:title" content="가상화폐 거래소 비교"/>
+<meta property="og:keywords" content="가상화폐 가격 비교, BTC, BTH, ETH, ETC, XRP, LTC, BTG, ZEC, 존버, 존버채팅, 김치프리미엄, 김프, 비트코인, 비트, 김프사이트, 리플, 이더리움"/>
