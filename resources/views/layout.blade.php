259fd2e6f27f5b0bc4a353c201f8e91d401ce8fc
dilee <dilee@trenditplatform.co.kr>
2018-02-27 17:22:01 +0900

    main #1xrp_please

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -16,7 +16,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
               </button>
-              <a class="navbar-brand" href="#">Zonber</a>
+              <a class="navbar-brand" href="#1xrp_please">Zonber</a>
             </div>
 
             <!-- Collect the nav links, forms, and other content for toggling -->
