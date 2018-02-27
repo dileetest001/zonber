42b15c94b7c4978ac314ec82cf1b2b52a69249da
dilee <dilee@trenditplatform.co.kr>
2018-02-27 17:47:17 +0900

    og image add

--- resources/views/common/head.blade.php
+++ resources/views/common/head.blade.php
@@ -7,6 +7,7 @@
 <meta property="og:type" content="website"/>
 <meta property="og:description" content="가상화폐 비트코인 존버들의 채팅방, 김치프리미엄 계산"/>
 <meta property="og:title" content="가상화폐 거래소 비교"/>
+<meta property="og:image" content="http://trenditplatform.co.kr/image/btc.png"/>
 <meta property="og:keywords" content="가상화폐 가격 비교, BTC, BTH, ETH, ETC, XRP, LTC, BTG, ZEC, 존버, 존버채팅, 김치프리미엄, 김프, 비트코인, 비트, 김프사이트, 리플, 이더리움"/>
 
 <link rel="apple-touch-icon" sizes="57x57" href="/favicon/apple-icon-57x57.png">
