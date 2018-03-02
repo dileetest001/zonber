8edba98c5733152d0f028489a2905289461f8ad1
dilee <dilee@trenditplatform.co.kr>
2018-03-02 16:44:48 +0900

    head 정보 변경

--- resources/views/common/head.blade.php
+++ resources/views/common/head.blade.php
@@ -1,14 +1,26 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+<title>김치프리미엄</title>
+
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 <meta name="csrf-token" content="{{ csrf_token() }}" />
 <meta name="keywords" content="가상화폐 가격 비교, BTC, BTH, ETH, ETC, XRP, LTC, BTG, ZEC, 존버, 존버채팅, 김치프리미엄, 김프, 비트코인, 비트, 김프사이트, 리플, 이더리움"/>
 <meta name="description" content="가상화폐 비트코인 존버들의 채팅방, 김치프리미엄 계산"/>
+<link rel="canonical" href="http://www.trenditplatform.co.k">
+
 <meta property="og:type" content="website"/>
 <meta property="og:description" content="가상화폐 비트코인 존버들의 채팅방, 김치프리미엄 계산"/>
-<meta property="og:title" content="가상화폐 거래소 비교"/>
-<meta property="og:image" content="http://trenditplatform.co.kr/image/btc.png"/>
+<meta property="og:title" content="김치프리미엄"/>
+<meta property="og:image" content="http://www.trenditplatform.co.kr/image/btc.png"/>
+<meta property="og:url" content="http://www.trenditplatform.co.kr">
 <meta property="og:keywords" content="가상화폐 가격 비교, BTC, BTH, ETH, ETC, XRP, LTC, BTG, ZEC, 존버, 존버채팅, 김치프리미엄, 김프, 비트코인, 비트, 김프사이트, 리플, 이더리움"/>
+
+<meta name="twitter:card" content="summary">
+<meta name="twitter:title" content="김치프리미엄">
+<meta name="twitter:description" content="가상화폐 비트코인 존버들의 채팅방, 김치프리미엄 계산">
+<meta name="twitter:image" content="http://www.trenditplatform.co.kr/image/btc.png">
+<meta name="twitter:domain" content="김치프리미엄">
 
 <link rel="apple-touch-icon" sizes="57x57" href="/favicon/apple-icon-57x57.png">
 <link rel="apple-touch-icon" sizes="60x60" href="/favicon/apple-icon-60x60.png">
@@ -28,6 +40,5 @@
 <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
 <meta name="theme-color" content="#ffffff">
 
-<title>김치프리미엄</title>
 <meta name="google-site-verification" content="fLPvxq2pnRYb36Y6NoC5fsX9f1kv9ohDl7HkbPQnnO4" />
 <meta name="naver-site-verification" content="609e62e5ae005b138562865341aaae4d6bb4931c"/>
